60da826a90718fb4d0eab403ba9f905663181887
Hala Abo Yousef <138528372+Halaabuyousef@users.noreply.github.com>
2026-03-13 15:26:38 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 document.getElementById("btn").addEventListener("click", () => {
     const msgElement = document.getElementById("msg");
-    msgElement.innerText = "Deployment Successful! CI/CD is working! ✅";
+    msgElement.innerText = "Deployment Successful! CI/CD is working! ";
     msgElement.style.color = "green";
     msgElement.style.fontWeight = "bold";
+
 });
